9817616c87650764c6ce89ecfee60742223b80d9
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-07-10 19:23:53 +0530

    Clean up

--- Tailwind-Practice/src/components/grid.jsx
+++ Tailwind-Practice/src/components/grid.jsx
@@ -1,17 +1,11 @@
-export default function Grid(){
+export default function Grid() {
     return (
-        // By default it stays in the left side
-        // sm:text-center makes it center in the screen size of 640px and above
-        <div className="grid grid-cols-12 text-left sm:text-center">
-            <div className="bg-red-500 col-span-7 p-2">
-                <h1>Hello World</h1>
-            </div>
-            <div className="bg-green-500 col-span-3 p-2">
-                <h1>Hello World</h1>
-            </div>
-            <div className="bg-blue-500 col-span-2 p-2">
-                <h1>Hello World</h1>
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-12 text-left sm:text-center">
+            {['red', 'green', 'blue'].map((color, index) => (
+                <div key={index} className={`bg-${color}-500 md:col-span-4 p-2`}>
+                    <h1>Hello World</h1>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+}   
