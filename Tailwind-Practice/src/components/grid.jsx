5ba246d2d0bbe8a7aa40a0e4f9ba5609d13d68ff
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-07-10 19:16:19 +0530

    Prefixing helper

--- Tailwind-Practice/src/components/grid.jsx
+++ Tailwind-Practice/src/components/grid.jsx
@@ -1,13 +1,15 @@
 export default function Grid(){
     return (
-        <div className="grid grid-cols-12 text-center">
-            <div className="bg-red-500 col-span-7">
+        // By default it stays in the left side
+        // sm:text-center makes it center in the screen size of 640px and above
+        <div className="grid grid-cols-12 text-left sm:text-center">
+            <div className="bg-red-500 col-span-7 p-2">
                 <h1>Hello World</h1>
             </div>
-            <div className="bg-green-500 col-span-3">
+            <div className="bg-green-500 col-span-3 p-2">
                 <h1>Hello World</h1>
             </div>
-            <div className="bg-blue-500 col-span-2">
+            <div className="bg-blue-500 col-span-2 p-2">
                 <h1>Hello World</h1>
             </div>
         </div>
