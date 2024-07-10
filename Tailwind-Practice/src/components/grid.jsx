fcbd5b629d066234d123fcc521e6e2a790af03af
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-07-10 19:02:16 +0530

    Minor upd

--- Tailwind-Practice/src/components/grid.jsx
+++ Tailwind-Practice/src/components/grid.jsx
@@ -1,13 +1,13 @@
 export default function Grid(){
     return (
-        <div className="grid grid-cols-4 text-center">
-            <div className="bg-red-500 col-span-1">
+        <div className="grid grid-cols-12 text-center">
+            <div className="bg-red-500 col-span-7">
                 <h1>Hello World</h1>
             </div>
-            <div className="bg-green-500 col-span-2">
+            <div className="bg-green-500 col-span-3">
                 <h1>Hello World</h1>
             </div>
-            <div className="bg-blue-500">
+            <div className="bg-blue-500 col-span-2">
                 <h1>Hello World</h1>
             </div>
         </div>
